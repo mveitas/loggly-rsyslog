eda10f562b2abb93b12c449cbb64b772bc3ec219
Matt Veitas <mveitas@gmail.com>
2014-01-28 09:23:17 -0500

    Bump version for 1.0.1 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mveitas@gmail.com'
 license          'Apache 2.0'
 description      'Configures rsyslog to send logs to Loggly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'ubuntu', '>= 12.04'
 
