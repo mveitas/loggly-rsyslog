5fa6cdb0bf5397cb4452382aaf76d227ec549a62
Adam Borocz <boroczadam@gmail.com>
2014-04-06 16:35:03 +0100

    Make the source of the Loggly token configurable
    Some people may not want to automatically load this from a data bag,
    and use attributes instead. Also, make the data bag and item names
    configurable.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,12 +3,20 @@
 # Recipe:: default
 #
 # Copyright (C) 2014 Matt Veitas
-# 
+#
 # All rights reserved - Do Not Redistribute
 #
 
-loggly_token = Chef::EncryptedDataBagItem.load('loggly', 'token')['token']
-raise "No token was found in the loggly databag." if loggly_token.nil?
+if node['loggly']['token']['from_databag']
+  databag = node['loggly']['token']['databag']
+  databag_item = node['loggly']['token']['databag_item']
+
+  loggly_token = Chef::EncryptedDataBagItem.load(databag, databag_item)['token']
+  raise "No token was found in databag item: #{databag}/#{databag_item}" if loggly_token.nil?
+  node.set['loggly']['token']['value'] = loggly_token
+else
+  raise "When not using a Data Bag, you have to define the Loggly token manually" if node['loggly']['token']['value'].empty?
+end
 
 include_recipe "rsyslog::default"
 
@@ -21,8 +29,7 @@ template node['loggly']['rsyslog']['conf'] do
   mode 0644
   variables({
     :monitor_files => !node['loggly']['log_files'].empty? || !node['loggly']['log_dirs'].empty?,
-    :tags => node['loggly']['tags'].nil? || node['loggly']['tags'].empty? ? '' : "tag=\\\"#{node['loggly']['tags'].join("\\\" tag=\\\"")}\\\"",
-    :token => loggly_token
+    :tags => node['loggly']['tags'].nil? || node['loggly']['tags'].empty? ? '' : "tag=\\\"#{node['loggly']['tags'].join("\\\" tag=\\\"")}\\\""
   })
   notifies :restart, "service[rsyslog]", :immediate
 end
